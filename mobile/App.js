17853bbe0121564274f1befab700929d23440308
Matheus Murbach <murbach@nappsolutions.com>
2019-11-26 14:10:50 -0300

    added project structure

--- mobile/App.js
+++ mobile/App.js
@@ -1,19 +1,68 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { createStackNavigator } from 'react-navigation-stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import {
+    createSwitchNavigator,
+    createAppContainer,
+  } from "react-navigation";
+
+import { Root } from "native-base";
+// import Login from './Login/Login';
+import MainScreen from './screens/Main';
+
+import { AsyncStorage, InteractionManager, ActivityIndicator } from "react-native";
+
+class AuthHandler extends Component {
+  handleAuth = async () => {
+    let routeName;
+
+    let token = await AsyncStorage.getItem('token')
+    if (token) {
+      routeName = "Default";
+    } else {
+      routeName = "Login";
+    }
+
+    this.props.navigation.navigate(routeName);
+  };
+
+  componentDidMount() {
+    InteractionManager.runAfterInteractions(this.handleAuth);
+  }
+
+  render() {
+    return <ActivityIndicator />;
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const Default = createStackNavigator(
+  {
+    General: { screen: MainScreen },
   },
-});
+  {
+    index: 0,
+    initialRouteName: "General",
+    headerMode: "none",
+  },
+);
+
+const Switch = createSwitchNavigator(
+  {
+    // AuthHandler: { screen: AuthHandler },
+    // Login: { screen: Login },
+    Default: { screen: Default },
+  },
+  {
+    index: 0,
+    initialRouteName: "Default",
+    headerMode: "none",
+  },
+);
+
+const App = createAppContainer(Switch);
+
+export default () => (
+  <Root>
+    <App renderLoadingExperimental={() => <ActivityIndicator />} />
+  </Root>
+);
---
+++ mobile/screens/Main/index.js
@@ -0,0 +1,88 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, Dimensions, Platform } from 'react-native';
+import { Content, Card, CardItem, Body } from 'native-base';
+import MapView, {Marker} from 'react-native-maps';
+import Constants from 'expo-constants';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
+
+export default class MainScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        location: null,
+        errorMessage: null
+    }
+  }
+
+  componentWillMount() {
+    if (Platform.OS === 'android' && !Constants.isDevice) {
+      this.setState({
+        errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
+      });
+    } else {
+      this._getLocationAsync();
+    }
+  }
+
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied',
+      });
+    }
+
+    let location = await Location.watchPositionAsync({
+    enableHighAccuracy:true
+        }, location => {
+            _this.setState({location});
+        });
+    console.log(location)
+    this.setState({ location });
+
+  };
+
+    
+  render() {
+    const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            backgroundColor: '#fff',
+            alignItems: 'center',
+            justifyContent: 'center',
+        },
+        mapStyle: {
+            width: Dimensions.get('window').width,
+            height: Dimensions.get('window').height + 25,
+        },
+    });
+    console.log(Location.watchPositionAsync())
+    return (
+      <>
+      <View
+        style={{
+          flex: 1,
+          flexDirection: 'column',
+        }}>
+        <MapView 
+            region={{
+                latitude:  this.state.location ? this.state.location.coords.latitude : 0,
+                longitude: this.state.location ? this.state.location.coords.longitude : 0,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+            }}
+            style={styles.mapStyle} 
+        >
+            <Marker
+                coordinate={{latitude: this.state.location ? this.state.location.coords.latitude : 0, longitude: this.state.location ? this.state.location.coords.longitude : 0}}
+                title="OPA"
+                description="VAI"
+            />
+        </MapView>
+      </View>
+      </>
+    );
+  }
+  
+}
